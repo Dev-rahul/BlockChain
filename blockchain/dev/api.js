c493964bf4354c50cfe52c17653aa16c9849eea9
Rahul <rahulpmurali@gmail.com>
2018-07-28 15:35:03 +0530

    nodemon + express.js

---
+++ blockchain/dev/api.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+app.get('/blockchain', function (req, res) {
+
+});
+
+app.post('/transacton',function(req, res) {
+
+});
+
+app.get('/mine',function(req, res) {
+
+});
+
+
+app.listen(3000, function() {
+   console.log('Listening to port 3000');
+})
--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -2,25 +2,5 @@ const Blockchain = require ('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = '8dbcnjsdnmvsd';
-const currentBlockData = [
-  {
-    amount: 10,
-    sender: 'N90dsadasdh',
-    recipient:'Bs7mdshds'
-  },
-  {
-    amount: 20,
-    sender: 'b90dsadasdh',
-    recipient:'Qs7mdshds'
 
-  },
-  {
-    amount: 30,
-    sender: 'b9DS0dsadasdh',
-    recipient:'QsF7mdshds'
-
-  },
-];
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 31381));
+console.log(bitcoin);
