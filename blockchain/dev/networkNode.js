252291699ef913320f5ba799aa40cee53023c143
Rahul <rahulpmurali@gmail.com>
2018-07-29 13:32:23 +0530

    bulk reg endpoint + all fixed

--- blockchain/dev/networkNode.js
+++ blockchain/dev/networkNode.js
@@ -83,7 +83,13 @@ app.post('/register-node', function(req, res) {
   });
 
 app.post('/register-nodes-bulk', function(req, res) {
-
+  const allNetworkNodes = req.body.allNetworkNodes;
+  allNetworkNodes.forEach(networkNodeUrl => {
+    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl) == -1;
+    const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+    if(nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(networkNodeUrl);
+  });
+  res.json({ note:'Bulk registration sucessfull.'});
 });
 
 
